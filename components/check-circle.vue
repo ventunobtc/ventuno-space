21602592bc87fac57a5e65f0d54081646be42a2f
Mauro Pili <mauro.pili@gmail.com>
2023-01-16 22:53:36 +0100

    feature wallet e nuovo componente check-circle

---
+++ components/check-circle.vue
@@ -0,0 +1,14 @@
+<template>
+   <div>
+    <b-icon v-if="attivo" icon="check-circle" size="is-medium"> </b-icon>
+    <b-icon v-else icon="panorama-fisheye" size="is-medium"> </b-icon>
+   </div> 
+</template>
+
+<script>
+export default {
+    props: {
+        attivo: Boolean
+    } 
+}
+</script>
