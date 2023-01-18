dfb4d9bda1c05e8ab568f7113efa55773d9a4c07
Mauro Pili <mauro.pili@gmail.com>
2023-01-18 23:37:11 +0100

    plugin v-tooltip per fonte su tabella wallet

--- components/check-circle.vue
+++ components/check-circle.vue
@@ -1,6 +1,6 @@
 <template>
    <div>
-    <b-icon v-if="attivo" icon="check-circle" size="is-medium"> </b-icon>
+    <b-icon v-if="attivo" icon="check-circle" size="is-medium" v-tooltip.hideOnTargetClick="{ content: linkafonte }"> </b-icon>
     <b-icon v-else icon="panorama-fisheye" size="is-medium"> </b-icon>
    </div> 
 </template>
@@ -8,7 +8,16 @@
 <script>
 export default {
     props: {
-        attivo: Boolean
-    } 
+        attivo: [String, Boolean]
+    },
+    computed: {
+        linkafonte(v) {
+            if (typeof this.attivo === 'string') {
+                return '<a href="' + this.attivo + '"  target="_blank">fonte</a>'
+            } else {
+                return null
+            }
+        }
+    }
 }
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -28,10 +28,12 @@ export default {
 
 	// Global CSS (https://go.nuxtjs.dev/config-css)
 	css: [
+		'~/assets/css/v-tooltip.css'
 	],
 
 	// Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
 	plugins: [
+		'~/plugins/v-tooltip.js'
 	],
 
 	// Auto import components (https://go.nuxtjs.dev/config-components)
